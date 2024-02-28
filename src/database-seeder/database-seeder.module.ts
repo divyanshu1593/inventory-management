3e2420357cf709284aa64127fc7cabe56b6e8eaf
jeelpatel231 <jeelpatel231@gmail.com>
2024-02-28 12:43:30 +0530

    implement product seeder

--- src/database-seeder/database-seeder.module.ts
+++ src/database-seeder/database-seeder.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseSeeder } from './database-seeder.service';
 import { UserSeeder } from './entity-seeders/user.seeder';
 import { RawMaterialSeeder } from './entity-seeders/raw-material.seeder';
+import { ProductSeeder } from './entity-seeders/product.seeder';
 
 @Module({
   providers: [
     DatabaseSeeder,
     UserSeeder,
     RawMaterialSeeder,
+    ProductSeeder,
     //
   ],
   exports: [DatabaseSeeder],
--- src/database-seeder/database-seeder.service.ts
+++ src/database-seeder/database-seeder.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { UserSeeder } from './entity-seeders/user.seeder';
 import { RawMaterialSeeder } from './entity-seeders/raw-material.seeder';
+import { ProductSeeder } from './entity-seeders/product.seeder';
 
 @Injectable()
 export class DatabaseSeeder {
   constructor(
     private readonly userSeeder: UserSeeder,
     private readonly rawMaterialSeeder: RawMaterialSeeder,
+    private readonly productSeeder: ProductSeeder,
   ) {}
 
   async seedAll() {
-    await Promise.all([
-      this.userSeeder.seed(),
-      this.rawMaterialSeeder.seed(),
-      //
-    ]);
+    const seeders = [
+      this.userSeeder,
+      this.rawMaterialSeeder,
+      this.productSeeder,
+    ];
+    await Promise.all(seeders.map((a) => a.seed()));
   }
 }

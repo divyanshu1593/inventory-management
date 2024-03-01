ceb2d2918817f83efc1ad3761dc0cfe3c5735460
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-03-01 12:08:10 +0530

    refactor code and throw valid exception on null user

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { User } from 'src/database/entities/user.entity';
@@ -17,14 +21,15 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(email: string, password: string): Promise<User> {
     const user = await this.userRepo.findOneBy({ email });
-    const passwordHash: boolean = await bcrypt.compare(
-      password,
-      user.passwordHash,
-    );
-    if (user && passwordHash) {
-      return user;
-    } else {
+    if (!user) {
+      throw new NotFoundException('User Not found in Database or not Approved');
+    }
+    const matched: boolean = await bcrypt.compare(password, user.passwordHash);
+
+    if (!matched) {
       throw new UnauthorizedException('Please check your login criteria');
     }
+
+    return user;
   }
 }

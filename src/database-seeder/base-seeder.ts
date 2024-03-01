c42c67251dbb9a7c60938ddfa5e1b623ca147054
jeelpatel231 <jeelpatel231@gmail.com>
2024-03-01 11:22:58 +0530

    factor out the generator function for public api

--- src/database-seeder/base-seeder.ts
+++ src/database-seeder/base-seeder.ts
@@ -5,11 +5,15 @@ export abstract class BaseSeeder<TEntity> {
 
   abstract generate(index: number): Promise<DeepPartial<TEntity>[]>;
 
+  async createEntry(index: number = 0): Promise<TEntity[]> {
+    const mock_entries = await this.generate(index);
+    const db_entries = mock_entries.map((e) => this.repo.create(e));
+    return await Promise.all(db_entries.map((entry) => this.repo.save(entry)));
+  }
+
   async seed() {
     for (let i = 0; i < 100; i++) {
-      const mock_entries = await this.generate(i);
-      const db_entries = mock_entries.map((e) => this.repo.create(e));
-      await Promise.all(db_entries.map((entry) => this.repo.save(entry)));
+      await this.createEntry(i);
     }
   }
 }

e3bc8d6b9df9a84162309b3b5db71310001a1fc3
jeelpatel231 <jeelpatel231@gmail.com>
2024-03-04 14:51:56 +0530

    - move to UUID for insertion - return the saved instances - optimise the query for production batch

--- src/manufacturing/manufacturing.service.ts
+++ src/manufacturing/manufacturing.service.ts
@@ -1,120 +1,80 @@
-import {
-  Injectable,
-  NotAcceptableException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { ProductDto } from './dto/product-info.dto';
 import { DataSource } from 'typeorm';
 import { Product } from 'src/database/entities/product.entity';
 import { ManufactureProductDto } from './dto/manufacture-product.dto';
 import { ProductionBatch } from 'src/database/entities/production-batch.entity';
 import { MachineConsumption } from 'src/database/entities/machine-consumption.entity';
-import { Machine } from 'src/database/entities/machine.entity';
-import { RawMaterial } from 'src/database/entities/raw-material.entity';
 import { tryWith } from 'src/database/error-handling/error-handler.adapter';
 import { SqliteForeignConstraint } from 'src/database/error-handling/handlers/foreign-key-constraint.handler';
 import { SqliteUniqueConstraint } from 'src/database/error-handling/handlers/unique-constraint.handler';
-import { EntityNotFoundHandler } from 'src/database/error-handling/handlers/entity-not-found.handler';
+import { RawMaterial } from 'src/database/entities/raw-material.entity';
 
 @Injectable()
 export class ManufacturingService {
   constructor(private readonly dataSource: DataSource) {}
 
-  async manufactureProduct(
-    manufactureProductDto: ManufactureProductDto,
-  ): Promise<void> {
-    const {
-      productName,
-      productModel,
-      productVariant,
-      machineName,
-      rawMaterialQuantityArray,
-    } = manufactureProductDto;
+  async manufactureProduct(manufactureProductDto: ManufactureProductDto) {
+    const { machineId, productId, rawMaterialQuantityArray } =
+      manufactureProductDto;
 
-    await this.dataSource.transaction(async (transactionalEntityManager) => {
-      const rawMaterialSet = new Set(
-        rawMaterialQuantityArray.map((val) => val.rawMaterialName),
-      );
-
-      if (rawMaterialSet.size !== rawMaterialQuantityArray.length) {
-        throw new NotAcceptableException(
-          'multiple raw material entry present in rawMaterialQuantityArray',
+    return await this.dataSource.transaction(
+      async (transactionalEntityManager) => {
+        const rawMaterialIds = rawMaterialQuantityArray.map(
+          (x) => x.rawMaterialId,
         );
-      }
 
-      const canConsume: { raw_material_name: string }[] =
-        await transactionalEntityManager
-          .createQueryBuilder(Machine, 'machine')
-          .innerJoinAndSelect('machine.consumes', 'raw_material')
-          .select('raw_material.name')
-          .where('machine.name = :machineName', { machineName })
-          .getRawMany();
+        const consumables = await transactionalEntityManager
+          .createQueryBuilder(RawMaterial, 'raw_material')
+          .innerJoinAndSelect(
+            'raw_material.consumed_by',
+            'machine',
+            'machine.id = :machineId and raw_material.id in (:...rawMaterialIds)',
+            { machineId, rawMaterialIds },
+          )
+          .getCount();
 
-      let itemMatchCnt = 0;
-      for (const rawMaterial of canConsume) {
-        if (rawMaterialSet.has(rawMaterial.raw_material_name)) {
-          itemMatchCnt++;
-        }
-      }
-
-      if (rawMaterialQuantityArray.length !== itemMatchCnt) {
-        throw new NotAcceptableException(
-          'some raw material cant be combined with provided machine',
-        );
-      }
-
-      const product = await tryWith(
-        transactionalEntityManager.findOneByOrFail(Product, {
-          name: productName,
-          model: productModel,
-          variant: productVariant,
-        }),
-      )
-        .onError(EntityNotFoundHandler, () => {
-          throw new NotFoundException('product not found');
-        })
-        .execute();
-
-      const batchInsertResult = await tryWith(
-        transactionalEntityManager.insert(ProductionBatch, {
-          product,
-        }),
-      )
-        .onError(SqliteForeignConstraint, () => {
+        if (consumables != rawMaterialIds.length) {
           throw new NotAcceptableException(
-            'product does not satisfy foriegn key constraint',
+            'some raw material cant be combined with provided machine',
           );
-        })
-        .execute();
+        }
 
-      // TODO: fix: only primanry keys should be passed as arguments
-      for (const rawMaterialQuantity of rawMaterialQuantityArray) {
-        await transactionalEntityManager.insert(MachineConsumption, {
-          batch: await transactionalEntityManager.findOneBy(ProductionBatch, {
-            id: batchInsertResult.identifiers[0].id,
+        const productionBatch = transactionalEntityManager.create(
+          ProductionBatch,
+          {
+            product: { id: productId },
+          },
+        );
+
+        await tryWith(transactionalEntityManager.save(productionBatch))
+          .onError(SqliteForeignConstraint, () => {
+            throw new NotAcceptableException(
+              'product does not satisfy foreign key constraint',
+            );
+          })
+          .execute();
+
+        const mapped = rawMaterialQuantityArray.map((rm) =>
+          transactionalEntityManager.create(MachineConsumption, {
+            batch: { id: productionBatch.id },
+            machine: { id: machineId },
+            raw_material: { id: rm.rawMaterialId },
+            raw_material_count: rm.amount,
           }),
-          machine: await transactionalEntityManager.findOneBy(Machine, {
-            name: machineName,
-          }),
-          raw_material: await transactionalEntityManager.findOneBy(
-            RawMaterial,
-            {
-              name: rawMaterialQuantity.rawMaterialName,
-            },
-          ),
-          raw_material_count: rawMaterialQuantity.amount,
-        });
-      }
-    });
+        );
+
+        return await transactionalEntityManager.save(mapped);
+      },
+    );
   }
 
-  async addProduct(productInfo: ProductDto): Promise<void> {
-    await tryWith(
-      this.dataSource.manager.insert(Product, {
-        ...productInfo,
-        amount: 0,
-      }),
-    )
+  async addProduct(productInfo: ProductDto) {
+    const created = this.dataSource.manager.create(Product, {
+      ...productInfo,
+      amount: 0,
+    });
+    return await tryWith(this.dataSource.manager.save(created))
       .onError(SqliteUniqueConstraint, () => {
         throw new NotAcceptableException('Product already exists');
       })

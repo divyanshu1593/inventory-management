3048d5c60517105b602b022bb304d4e1f0038c89
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-03-04 11:26:57 +0530

    mapped splite errors to http exceptions

--- src/manufacturing/manufacturing.service.ts
+++ src/manufacturing/manufacturing.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import {
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
 import { ProductDto } from './dto/product-info.dto';
 import { DataSource } from 'typeorm';
 import { Product } from 'src/database/entities/product.entity';
@@ -7,6 +11,10 @@ import { ProductionBatch } from 'src/database/entities/production-batch.entity';
 import { MachineConsumption } from 'src/database/entities/machine-consumption.entity';
 import { Machine } from 'src/database/entities/machine.entity';
 import { RawMaterial } from 'src/database/entities/raw-material.entity';
+import { tryWith } from 'src/database/error-handling/error-handler.adapter';
+import { SqliteForeignConstraint } from 'src/database/error-handling/handlers/foreign-key-constraint.handler';
+import { SqliteUniqueConstraint } from 'src/database/error-handling/handlers/unique-constraint.handler';
+import { EntityNotFoundHandler } from 'src/database/error-handling/handlers/entity-not-found.handler';
 
 @Injectable()
 export class ManufacturingService {
@@ -55,19 +63,31 @@ export class ManufacturingService {
         );
       }
 
-      const product = await transactionalEntityManager.findOneBy(Product, {
-        name: productName,
-        model: productModel,
-        variant: productVariant,
-      });
+      const product = await tryWith(
+        transactionalEntityManager.findOneByOrFail(Product, {
+          name: productName,
+          model: productModel,
+          variant: productVariant,
+        }),
+      )
+        .onError(EntityNotFoundHandler, () => {
+          throw new NotFoundException('product not found');
+        })
+        .execute();
 
-      const batchInsertResult = await transactionalEntityManager.insert(
-        ProductionBatch,
-        {
+      const batchInsertResult = await tryWith(
+        transactionalEntityManager.insert(ProductionBatch, {
           product,
-        },
-      );
+        }),
+      )
+        .onError(SqliteForeignConstraint, () => {
+          throw new NotAcceptableException(
+            'product does not satisfy foriegn key constraint',
+          );
+        })
+        .execute();
 
+      // TODO: fix: only primanry keys should be passed as arguments
       for (const rawMaterialQuantity of rawMaterialQuantityArray) {
         await transactionalEntityManager.insert(MachineConsumption, {
           batch: await transactionalEntityManager.findOneBy(ProductionBatch, {
@@ -89,9 +109,15 @@ export class ManufacturingService {
   }
 
   async addProduct(productInfo: ProductDto): Promise<void> {
-    await this.dataSource.manager.insert(Product, {
-      ...productInfo,
-      amount: 0,
-    });
+    await tryWith(
+      this.dataSource.manager.insert(Product, {
+        ...productInfo,
+        amount: 0,
+      }),
+    )
+      .onError(SqliteUniqueConstraint, () => {
+        throw new NotAcceptableException('Product already exists');
+      })
+      .execute();
   }
 }

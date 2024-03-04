9b1847dc669464fb9a834035dc73798ac4b7bf2c
jeelpatel231 <jeelpatel231@gmail.com>
2024-03-04 18:10:05 +0530

    factor out typeorm module and seeding module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AllEntities } from './database';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/config.env';
 import { SalesModule } from './sales/sales.module';
 import { DatabaseModule } from './database/database.module';
-import { DatabaseSeedingModule } from './database-seeder/database-seeder.module';
 import { ManufacturingModule } from './manufacturing/manufacturing.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './config/config.module';
 import { ProcurementModule } from './procurement/procurement.module';
+import { TypeOrmConnectionModule } from './database/typeorm-root.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ validate }),
     AppConfigModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite3',
-      entities: AllEntities,
-      synchronize: true,
-    }),
+    TypeOrmConnectionModule,
     SalesModule,
     DatabaseModule,
-    DatabaseSeedingModule,
     ManufacturingModule,
     AuthModule,
     ProcurementModule,
--- src/seed.ts
+++ src/seed.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DatabaseSeeder } from './database-seeder/database-seeder.service';
-import { Logger } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { TypeOrmConnectionModule } from './database/typeorm-root.module';
+import { DatabaseModule } from './database/database.module';
+import { DatabaseSeedingModule } from './database-seeder/database-seeder.module';
+
+@Module({
+  imports: [TypeOrmConnectionModule, DatabaseModule, DatabaseSeedingModule],
+})
+class SeedModule {}
 
 async function seed() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(SeedModule);
   const seeder = app.get(DatabaseSeeder);
   const logger = new Logger('Seeding Task');
   logger.log('Seeding started');

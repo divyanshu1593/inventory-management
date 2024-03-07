1b5f26084c9b1b6aacd490aeece1ae958be7ed1d
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-03-07 15:53:36 +0530

    added appropriate decorators

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -2,9 +2,9 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { CreateSalesDto } from './dto/create-sales.dto';
 import { UUID } from 'crypto';
-import { AllowDept } from 'src/guards/department.guard';
+import { AllowAllDept, AllowDept } from 'src/guards/department.guard';
 import { CompanyDepartment } from 'src/database/entities/company-departments';
-import { AllowRoles } from 'src/guards/roles.guard';
+import { AllowAllRoles, AllowRoles } from 'src/guards/roles.guard';
 import { UserRole } from 'src/database/entities/user.roles';
 
 @AllowDept(CompanyDepartment.SALES)
@@ -13,24 +13,27 @@ export class SalesController {
   constructor(private readonly salesService: SalesService) {}
 
   @AllowRoles(UserRole.OPERATOR)
-  @Post()
-  createSales(@Body() createSalesDto: CreateSalesDto) {
+  @Post('create-sale')
+  createSale(@Body() createSalesDto: CreateSalesDto) {
     return this.salesService.createSale(createSalesDto);
   }
 
-  @AllowRoles(UserRole.MANAGER)
-  @Get()
+  @AllowAllRoles()
+  @AllowAllDept()
+  @Get('get-sales')
   getSales() {
     return this.salesService.getSales();
   }
 
-  @AllowRoles(UserRole.MANAGER)
+  @AllowAllRoles()
+  @AllowAllDept()
   @Get('id/:id')
   getSaleById(@Param('id') productId: UUID) {
     return this.salesService.getSaleById(productId);
   }
 
-  @AllowRoles(UserRole.MANAGER)
+  @AllowAllRoles()
+  @AllowAllDept()
   @Get('totalcost/:totalCost')
   getSalesByTotalCost(@Param('totalCost') totalCost: number) {
     return this.salesService.getSalesByTotalCost(totalCost);
